c7790efabc2655089b0c58ac62cdb64703e2d0ef
Jez-er <andrey.chernenko008@gmail.com>
2024-12-25 18:37:46 +0200

    [ADD] Activating voice

--- stt/listen.py
+++ stt/listen.py
@@ -1,11 +1,23 @@
 import speech_recognition as sr
+from tts.gtts import gtts_local
+from pathlib import Path
 
 # Ініціалізація розпізнавача
 recognizer = sr.Recognizer()
 
 # Використання мікрофона
 def Listen():
+
+	current_directory = Path(__file__).parent
+
+	# Створюємо відносний шлях до файлу
+	file_path = current_directory / '..' / 'assets' / 'active.mp3'
+
+	# Перетворюємо шлях на абсолютний
+	file_path = file_path.resolve()
+
 	with sr.Microphone() as source:
+			gtts_local(file_path)
 			print("Скажіть щось українською...")
 			recognizer.adjust_for_ambient_noise(source)  # Налаштування на шум
 			audio = recognizer.listen(source)  # Запис звуку
@@ -15,6 +27,6 @@ def Listen():
 					text = recognizer.recognize_google(audio, language="uk-UA")
 					return text
 			except sr.UnknownValueError:
-					print("Не вдалося розпізнати мову.")
+					return False
 			except sr.RequestError as e:
 					print(f"Помилка сервісу: {e}")
